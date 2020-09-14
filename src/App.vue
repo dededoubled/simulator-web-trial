57732c7bcb38acc496b20f811dcb3252dd8a2923
dede agus setiawan <gubrak.sukur9@gmail.com>
2020-09-14 17:05:12 +0700

    konfigurasi tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="bg-red-600 ml-">button</button>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
