7f52b93cd260684ae1c0c9d27294f7c9e0b5a4a6
dede agus setiawan <gubrak.sukur9@gmail.com>
2020-09-15 12:16:33 +0700

    mengganti logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,12 @@
    <div class="bg-d-rhino py-4">
       <header class="flex justify-between items-center md:max-w-6xl md:mx-auto px-4">
          <a href="#">
-            <img src="../assets/logo-vue.svg" alt="" class="w-10 sm:w-12">
+            <img src="https://res.cloudinary.com/dededoubled/image/upload/v1586530715/doubled.png" alt="" class="w-10 sm:w-16">
          </a>
-         <div class="sm:flex sm:flex-row hidden"> 
-            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">features</a>
-            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">pricing</a>
-            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">log in</a>
+         <div class="sm:flex sm:flex-row hidden font-montserrat"> 
+            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">list game</a>
+            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">mods</a>
+            <a href="#" class="text-lg px-3 py-1 font-semibold text-white">kontak</a>
          </div>
       </header>
    </div>
